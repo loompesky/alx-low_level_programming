1968a23208af5e0e8457ae4a13e717402bb1ca90
loompesky <loomesky@yahoo.com>
2022-10-14 15:07:32 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,7 +13,7 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = ramd() - RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 
 	if (n % 10 > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
@@ -24,4 +24,3 @@ int main(void)
 
 	return (0);
 }
-
