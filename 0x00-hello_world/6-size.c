54e22db93aa852e477d3d06c6080804c55bfe401
loompesky <loomesky@yahoo.com>
2022-10-13 17:15:54 -0700

    size not grandeur

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n",(unsigned long)szeof(d));
+	printf("Size of an int: %lu byte(s)\n",(unsigned long)szeof(a));
+	printf("Size of a long int: %lu byte(s)\n",(unsigned long)szeof(b));
+	printf("Size of a long long int: %lu byte(s)\n",(unsigned long)szeof(c));
+	printf("Size of a float: %lu byte(s)\n",(unsigned long)szeof(f));
+	return (o);
+}
