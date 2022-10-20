fa1beafd438004de91cb085cbbbb099a90aafa3b
loompesky <loomesky@yahoo.com>
2022-10-20 15:56:32 -0700

    test the function is positive or negative

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,15 @@
+#ifndef MATH_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/*The function test if an integer is positive or negative*/
+void positive_or_negative(int i);
+
+int largest_number(int a, int b, int c);
+
+int convert_day(int month, int day);
+
+void print_remaining_days(int month, int day, int year);
+
+#endif /* MAIN_H */
