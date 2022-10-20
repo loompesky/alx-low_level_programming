f8b0016ec62b19656d43c27235799a09002e640b
loompesky <loomesky@yahoo.com>
2022-10-20 16:39:58 -0700

    largest number of three numbers

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,6 +6,7 @@
 /*The function test if an integer is positive or negative*/
 void positive_or_negative(int i);
 
+/*The function that returns the largest of 3 numbers*/
 int largest_number(int a, int b, int c);
 
 int convert_day(int month, int day);
