d27285fc36b8a2eee8d06312f16ddc9015ff7ce7
loompesky <loomesky@yahoo.com>
2022-10-20 17:01:41 -0700

    leap year

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -11,6 +11,7 @@ int largest_number(int a, int b, int c);
 
 int convert_day(int month, int day);
 
+/*The function that prints leap year*/
 void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
